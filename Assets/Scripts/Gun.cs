f73dc7aabb5bd5270f8db3fbec986e1b330652f9
ivan <mihastefa2605@gmail.com>
2026-08-17 15:40:24 +0200

    finalen proekt

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] GameObject projectilePrefab;
+
+    private XRGrabInteractableFixed grabInteractableFixed;
+
+    private void Awake()
+    {
+        grabInteractableFixed = GetComponent<XRGrabInteractableFixed>();
+    }
+
+    void Start()
+    {
+        grabInteractableFixed.activated.AddListener(Shoot);
+    }
+
+    private void Shoot(ActivateEventArgs arg0)
+    {
+        Instantiate(projectilePrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+    private void OnDestroy()
+    {
+        grabInteractableFixed.activated.RemoveListener(Shoot);
+    }
+}
---
+++ Assets/Scripts/XRGrabInteractableFixed.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class XRGrabInteractableFixed : XRGrabInteractable
+{
+    [Header("Primary Attach Transforms")]
+    [SerializeField] private Transform leftHandAttachTransform;
+    [SerializeField] private Transform rightHandAttachTransform;
+
+    protected override void OnSelectEntering(SelectEnterEventArgs args)
+    {
+        if (args.interactorObject.handedness == InteractorHandedness.Left)
+            attachTransform = leftHandAttachTransform;
+        else
+            attachTransform = rightHandAttachTransform;
+
+        base.OnSelectEntering(args);
+    }
+}
